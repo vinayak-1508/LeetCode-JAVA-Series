75841652410b56389e3c5244b1577f9fb38df52c
vinayak-1508 <130927218+vinayak-1508@users.noreply.github.com>
2024-01-02 22:17:18 +0530

    Time: 3 ms (62.30%), Space: 43.6 MB (57.04%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        return solve(board);
+    }
+     boolean solve(char[][]board)
+    { for(int i=0;i<board.length;i++)
+    {for(int j=0;j<board.length;j++)
+    { if(board[i][j] != '.')
+    { char num=board[i][j];
+     board[i][j]='.';
+     if(!isSafe(board,i,j,num)) return false;
+      else
+    
+     board[i][j]=num;
+     } }}
+    return true; }
+     boolean isSafe(char[][] board,int row,int col,char num)
+    { 
+      for(int i=0;i<board.length;i++)
+      { 
+        if(board[i][col] == num)  return false;
+         
+        if(board[row][i]== num) return false;
+      }int sqrt=(int)(Math.sqrt(board.length));
+      int rowStart=row - row % sqrt;
+      int colStart=col - col % sqrt;
+      for(int r=rowStart;r<rowStart + sqrt;r++)
+      { for(int c=colStart;c<colStart + sqrt;c++)
+       {if(board[r][c] == num)   return false; }
+      }return true;
+}
+}
